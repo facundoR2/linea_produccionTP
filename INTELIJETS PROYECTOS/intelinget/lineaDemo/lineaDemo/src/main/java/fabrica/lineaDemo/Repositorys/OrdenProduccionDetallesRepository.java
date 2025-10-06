40733f3664b64b8bf03b1931bce8fb80aa491ce2
facundoR2 <facundo_rios1@hotmail.com>
2025-10-06 16:45:49 -0300

    repositorio para las orden de produccion

--- INTELIJETS PROYECTOS/intelinget/lineaDemo/lineaDemo/src/main/java/fabrica/lineaDemo/Repositorys/OrdenProduccionDetallesRepository.java
+++ INTELIJETS PROYECTOS/intelinget/lineaDemo/lineaDemo/src/main/java/fabrica/lineaDemo/Repositorys/OrdenProduccionDetallesRepository.java
@@ -1,9 +1,13 @@
 package fabrica.lineaDemo.Repositorys;
 
 import fabrica.lineaDemo.Models.OrdenProduccionDetalles;
+import fabrica.lineaDemo.Models.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrdenProduccionDetallesRepository extends JpaRepository<OrdenProduccionDetalles,Integer> {
+
+
+    OrdenProduccionDetalles findByProducto(Producto producto);
 }
